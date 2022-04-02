4fa21a80b0b361db09aa6679239180918ed3cb6e
legion <88172317+LegacyOO7@users.noreply.github.com>
2022-04-02 07:19:48 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -97,8 +97,8 @@ async def start_command(client: Client, message: Message):
                     InlineKeyboardButton("⚡ 𝗖𝗹𝗼𝘀𝗲 ⚡", callback_data = "close")
                 ],
                 [
-                    InlineKeyboardButton("⚡ 𝙎𝙚𝙧𝙞𝙚𝙨𝙊𝙢𝙚𝙜𝙖 ⚡", url = "https://t.me/FuZionX"),
-                    InlineKeyboardButton("⚡ 𝙊𝙏𝙏 𝙐𝙋𝘿𝘼𝙏𝙀𝙎 ⚡", url = "https://t.me/FuZionXGroup")
+                    InlineKeyboardButton("⚡ 𝙎𝙚𝙧𝙞𝙚𝙨𝙊𝙢𝙚𝙜𝙖 ⚡", url = "https://t.me/Movies_updates_MT"),
+                    InlineKeyboardButton("⚡ 𝙊𝙏𝙏 𝙐𝙋𝘿𝘼𝙏𝙀𝙎 ⚡", url = "https://t.me/+THx-dmN7OI85NzBl")
                 ]
             ]
         )
